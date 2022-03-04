046d38d3ce2693e8f222f50978ccb2d7ac2fe422
Garrett Peake <gepeake@gmail.com>
2022-03-04 15:09:10 -0800

    gp - Add post for week 2

---
+++ src/routes/posts/2022-03-04-ECE153BWeek2/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Tilt Game Week 2`,
+  tags: ['ECE153B', 'tiltgame'],
+  spoiler: "ECE153B Final Project weekly update #2",
+  getContent: () => import('./document.mdx'),
+}
